ac07d37ef9a6a838b1e576b7e4f37336f770c104
Luke MacLean <40969439+MacLeanLuke@users.noreply.github.com>
2021-01-24 19:22:06 -0500

    Tonights list of things for pinky to do

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-//Create a react app from scratch.
-//It should display a h1 heading.
-//It should display an unordered list (bullet points).
-//It should contain 3 list elements.
+const React = require("react");
+const ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>
+    <h1>Tonight's ToDos!</h1>
+    <ul>
+      <li>The Same Thing</li>
+      <li>We do every night</li>
+      <li>Try to take over the world</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
